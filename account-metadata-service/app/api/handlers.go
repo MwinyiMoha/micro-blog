831642ded7a9529a671080ca8b36733ccf308276
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2021-01-05 19:34:19 +0300

    AccMeta: Set up handlers

--- account-metadata-service/app/api/handlers.go
+++ account-metadata-service/app/api/handlers.go
@@ -1,6 +1,20 @@
 package api
 
-// GetAccountMeta is a route handler, that given a user ID, gets metadata for that user
-func GetAccountMeta() {
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
+// GetAccountMeta is a route handler, that given a user ID, gets metadata for that user
+func GetAccountMeta() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"detail": "Retrieved"})
+	}
+}
+
+// CreateAccountMeta is a route handler that creates a metadata object for a user
+func CreateAccountMeta() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"detail": "Created"})
+	}
 }
--- account-metadata-service/app/main.go
+++ account-metadata-service/app/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"account-metadata-service/app/api"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"detail": "Account Metadata Service"})
+}
+
 func main() {
-	fmt.Println("Hello service")
+	r := gin.Default()
+
+	r.GET("/", rootHandler)
+	r.GET("/metadata", api.GetAccountMeta())
+	r.POST("/metadata", api.CreateAccountMeta())
+
+	r.Run(":6000")
 }
